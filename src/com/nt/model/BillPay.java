eeaf583d33169ac74a550ee20c99e4de99b54066
PraveenP9 <praveenpattem6@gmail.com>
2020-02-11 18:05:55 +0530

    Constructor added

--- src/com/nt/model/BillPay.java
+++ src/com/nt/model/BillPay.java
@@ -1,9 +1,14 @@
 package com.nt.model;
 
 public class BillPay {
+	public BillPay() {
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Praveen");
 		System.out.println("From Bill Pay");
+
 	}
 
 }
